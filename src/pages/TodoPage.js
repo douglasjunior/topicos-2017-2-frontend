56eaf692eb080a2fbeeef016424f757b72b0ccb4
Douglas Nassif Roma Junior <nassifrroma@gmail.com>
2017-09-06 08:03:54 -0300

    Criado tabela HTML para listagem das Todos.

--- src/pages/TodoPage.js
+++ src/pages/TodoPage.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 
 import axios from 'axios';
 
+import {
+    Table, Button,
+    Modal, ButtonGroup
+} from 'react-bootstrap';
+
 class TodoPage extends Component {
 
     state = {
@@ -20,15 +25,56 @@ class TodoPage extends Component {
             })
     }
 
+    onExcluirClick = (todo) => {
+        window.confirm("Deseja excluir a tarefa " + todo.id + "?");
+    }
+
+    renderTodo = () => {
+        const todos = this.state.todos;
+
+        const todosComponents = todos.map((todo, index) => {
+            return (
+                <tr>
+                    <td>{todo.id}</td>
+                    <td>{todo.title}</td>
+                    <td>{todo.date}</td>
+                    <td>{todo.completed}</td>
+                    <td>
+                        <ButtonGroup bsSize="small">
+                            <Button bsStyle="warning">Editar</Button>
+                            <Button bsStyle="danger"
+                                onClick={() => this.onExcluirClick(todo)}>
+                                Excluir
+                            </Button>
+                        </ButtonGroup>
+                    </td>
+                </tr>
+            );
+        });
+
+        return todosComponents;
+    }
+
     render() {
         const todos = this.state.todos;
-        const todosComponents = todos.map((todo, index) => {
-            return <p>{todo.id} - {todo.title}</p>;
-        })
+
         return (
             <section>
                 <h1>Página de Tarefas</h1>
-                {todosComponents}
+                <Table>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Título</th>
+                            <th>Data</th>
+                            <th>Concluída</th>
+                            <th>Options</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.renderTodo()}
+                    </tbody>
+                </Table>
             </section>
         );
     }
